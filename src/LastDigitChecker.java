1d56c10b7f19c1c0a2cc4427c2cd9703845d38db
Kaung-Htet <kaunghtetnyilay@gmail.com>
2020-06-22 18:48:31 +0700

    Add hasSameLastDigit method

--- src/LastDigitChecker.java
+++ src/LastDigitChecker.java
@@ -6,11 +6,28 @@ public class LastDigitChecker {
             return true;
         }
     }
+
+    public static boolean hasSameLastDigit (int num1, int num2, int num3){
+        if (isValid(num1) && isValid(num2) && isValid(num3)){
+                if ((num1 % 10) == (num2 % 10)){
+                    return true;
+                } else if ((num1 % 10) == (num3 % 10)){
+                    return true;
+                } else if((num2 % 10) == (num3 % 10)){
+                    return true;
+                }else {
+                    return false;
+                }
+        } else {
+            return false;
+        }
+    }
 }
 
 //Write a method named hasSameLastDigit with three parameters of type int.
 //
-//Each number should be within the range of 10 (inclusive) - 1000 (inclusive). If one of the numbers is not within the range, the method should return false.
+//Each number should be within the range of 10 (inclusive) - 1000 (inclusive).
+//If one of the numbers is not within the range, the method should return false.
 //
 //The method should return true if at least two of the numbers share the same rightmost digit; otherwise, it should return false.
 //
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(LastDigitChecker.isValid(9));
+        System.out.println(LastDigitChecker.hasSameLastDigit(41, 22, 71));
+        System.out.println(LastDigitChecker.hasSameLastDigit(23, 32, 42));
+        System.out.println(LastDigitChecker.hasSameLastDigit(9, 99, 999));
+
+        System.out.println(LastDigitChecker.isValid(10));
+        System.out.println(LastDigitChecker.isValid(468));
+        System.out.println(LastDigitChecker.isValid(1051));
     }
 }
+
+//* hasSameLastDigit (41, 22, 71); → should return true since 1 is the rightmost digit in numbers 41 and 71
+//
+//* hasSameLastDigit (23, 32, 42); → should return true since 2 is the rightmost digit in numbers 32 and 42
+//
+//* hasSameLastDigit (9, 99, 999); → should return false since 9 is not within the range of 10-1000
+
+//* isValid(10); → should return true since 10 is within the range of 10-1000
+//
+//* isValid(468); → should return true since 468 is within the range of 10-1000
+//
+//* isValid(1051); → should return false since 1051 is not within the range of 10-1000
